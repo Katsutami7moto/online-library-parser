f5e8d62f00fe14c2d48c6f7f05f897c368040494
Katsutami7moto <nclsf87@gmail.com>
2022-06-22 18:37:43 +0300

    Download books from tululu.org

--- main.py
+++ main.py
@@ -1,7 +1,25 @@
+import os
+from pathlib import Path
+
 import requests
 
-url = 'https://tululu.org/txt.php?id=32168'
-response = requests.get(url)
-response.raise_for_status()
-with open('book.txt', 'w') as file:
-    file.write(response.text)
+books_path = 'books'
+books_dir = Path(books_path)
+books_dir.mkdir(parents=True, exist_ok=True)
+
+start_number = 32168
+books = []
+while len(books) < 10:
+    file_number = len(books) + 1
+    book_number = start_number + file_number
+    url = f'https://tululu.org/txt.php?id={book_number}'
+    response = requests.get(url)
+    try:
+        response.raise_for_status()
+        file_name = f'id{file_number}.txt'
+        file_path = books_dir.joinpath(file_name)
+        with open(file_path, 'w') as file:
+            file.write(response.text)
+    except Exception as e:
+        print(e)
+    books = tuple(os.walk(books_path))[0][2]
