ed820ddf3a6b8c031275ac273334191d56808fa2
Katsutami7moto <nclsf87@gmail.com>
2022-06-23 15:44:45 +0300

    Get images URLS

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from urllib.parse import urljoin
 
 import requests
 from bs4 import BeautifulSoup
@@ -6,25 +7,32 @@ from pathvalidate import sanitize_filename
 
 
 def check_for_redirect(response: requests.Response, number: int):
-    redirect = response.history and '?' not in response.url
+    redirect = response.history
     if redirect:
         err_msg = f'There is no book with ID {number}.'
         raise requests.HTTPError(err_msg)
 
 
 def get_books_title(book_id: int) -> tuple:
-    url = f'https://tululu.org/b{book_id}'
-    response = requests.get(url)
+    base_url = 'https://tululu.org'
+    book_url = urljoin(base_url, f'b{book_id}')
+    response = requests.get(book_url)
     response.raise_for_status()
     soup = BeautifulSoup(response.text, 'lxml')
     title_soup_text = soup.find('html').find('head').find('title').text
+    if ' - ' not in title_soup_text:
+        return '', ''
     title, author = title_soup_text.split(' - ')
     title: str = title.strip()
-    author: str = author.split(',')[0].strip()
-    return title, author
+    # author: str = author.split(',')[0].strip()
+    image_soup = soup.find('body').find('div', class_='bookimage').find('img')
+    image_url = urljoin(base_url, image_soup.attrs['src'])
+    requests.get(image_url).raise_for_status()
+    return title, image_url
 
 
-def download_txt(url: str, books_dir: Path, file_number: int) -> str:
+def download_txt(books_dir: Path, file_number: int) -> str:
+    url = 'https://tululu.org/txt.php'
     payload = {'id': file_number}
     response = requests.get(url, params=payload)
     response.raise_for_status()
@@ -42,12 +50,15 @@ def download_books_with_titles(folder: str = 'books'):
     books_dir.mkdir(parents=True, exist_ok=True)
     for number in range(10):
         file_number = number + 1
-        url = 'https://tululu.org/txt.php'
         try:
-            download_txt(url, books_dir, file_number)
+            # download_txt(url, books_dir, file_number)
+            title, img_src = get_books_title(file_number)
+            if title and img_src:
+                print(f'Заголовок: {title}\n{img_src}')
         except requests.HTTPError as err:
             print(err)
         finally:
+            print()
             continue
 
 
