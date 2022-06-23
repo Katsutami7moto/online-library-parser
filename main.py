fad6c52b67e45360319cd067a785d3b53191a9ce
Katsutami7moto <nclsf87@gmail.com>
2022-06-23 10:59:50 +0300

    Handle redirects

--- main.py
+++ main.py
@@ -1,25 +1,35 @@
-import os
 from pathlib import Path
 
 import requests
 
-books_path = 'books'
-books_dir = Path(books_path)
-books_dir.mkdir(parents=True, exist_ok=True)
 
-start_number = 32168
-books = []
-while len(books) < 10:
-    file_number = len(books) + 1
-    book_number = start_number + file_number
-    url = f'https://tululu.org/txt.php?id={book_number}'
-    response = requests.get(url)
-    try:
-        response.raise_for_status()
-        file_name = f'id{file_number}.txt'
-        file_path = books_dir.joinpath(file_name)
-        with open(file_path, 'w') as file:
-            file.write(response.text)
-    except Exception as e:
-        print(e)
-    books = tuple(os.walk(books_path))[0][2]
+def check_for_redirect(response: requests.Response):
+    redirect = response.history and '?' not in response.url
+    if redirect:
+        raise requests.HTTPError
+
+
+def main():
+    books_path = 'books'
+    books_dir = Path(books_path)
+    books_dir.mkdir(parents=True, exist_ok=True)
+
+    for number in range(10):
+        file_number = number + 1
+        payload = {'id': file_number}
+        url = 'https://tululu.org/txt.php'
+        response = requests.get(url, params=payload)
+        try:
+            response.raise_for_status()
+            check_for_redirect(response)
+        except requests.HTTPError:
+            pass
+        else:
+            file_name = f'id{file_number}.txt'
+            file_path = books_dir.joinpath(file_name)
+            with open(file_path, 'w') as file:
+                file.write(response.text)
+
+
+if __name__ == '__main__':
+    main()
