a0c13a62de49fb2ad1bd0e8712a53bec8174a8c8
Katsutami7moto <nclsf87@gmail.com>
2022-06-29 12:09:17 +0300

    Download books from all pages

--- parse_tululu.py
+++ parse_tululu.py
@@ -8,9 +8,19 @@ from bs4 import BeautifulSoup
 from pathvalidate import sanitize_filename
 
 
-def check_for_redirect(response: requests.Response, number: int):
+def create_book_path(books_dir: Path, book_title: str) -> Path:
+    file_name = f'{sanitize_filename(book_title)}.txt'
+    return books_dir.joinpath(file_name)
+
+
+def create_image_path(images_dir: Path, image_url: str) -> Path:
+    file_name = sanitize_filename(get_file_name_from_url(image_url))
+    return images_dir.joinpath(file_name)
+
+
+def check_for_redirect(response: requests.Response, book_id):
     if response.history:
-        err_msg = f'There is no book with ID {number}'
+        err_msg = f'There is no book with ID {book_id}'
         raise requests.HTTPError(err_msg)
 
 
@@ -18,7 +28,7 @@ def get_file_name_from_url(url: str) -> str:
     return Path(unquote(urlsplit(url).path)).name
 
 
-def get_book_soup(book_id: int) -> BeautifulSoup:
+def get_book_soup(book_id) -> BeautifulSoup:
     base_url = 'https://tululu.org'
 
     # book_url has to end with '/', or else check_for_redirect() won't work
@@ -57,56 +67,62 @@ def get_book_genres(book_soup: BeautifulSoup) -> list:
 
 def parse_book_page(book_soup: BeautifulSoup) -> dict:
     title, author = get_book_title_and_author(book_soup)
-    image_url = get_image_url(book_soup)
-    comments = get_book_comments(book_soup)
     genres = get_book_genres(book_soup)
+    comments = get_book_comments(book_soup)
     parsed_book = {
         'title': title,
         'author': author,
+        'image_path': '',
+        'book_path': '',
         'genres': genres,
-        'image': image_url,
         'comments': comments,
     }
     return parsed_book
 
 
-def download_txt(books_dir: Path, book_id: int, book_title: str):
+def download_txt(book_path: Path, book_id):
     base_url = 'https://tululu.org/txt.php'
     payload = {'id': book_id}
     response = requests.get(base_url, params=payload)
     response.raise_for_status()
     check_for_redirect(response, book_id)
-    file_name = f'{book_id}. {sanitize_filename(book_title)}.txt'
-    file_path = books_dir.joinpath(file_name)
-    with open(file_path, 'w') as file:
+    with open(book_path, 'w') as file:
         file.write(response.text)
 
 
-def download_image(images_dir: Path, url: str):
+def download_image(image_path: Path, url: str):
     response = requests.get(url)
     response.raise_for_status()
-    file_name = sanitize_filename(get_file_name_from_url(url))
-    file_path = images_dir.joinpath(file_name)
-    with open(file_path, 'wb') as file:
+    with open(image_path, 'wb') as file:
         file.write(response.content)
 
 
-def download_books_and_images(start: int, end: int):
+def download_books_and_images(book_ids) -> list:
     books_dir = Path('books')
     books_dir.mkdir(parents=True, exist_ok=True)
     images_dir = Path('images')
     images_dir.mkdir(parents=True, exist_ok=True)
-    for book_id in range(start, end + 1):
+    parsed_books = []
+    for book_id in book_ids:
         first_reconnection = True
         while True:
             try:
                 book_soup = get_book_soup(book_id)
                 parsed_book = parse_book_page(book_soup)
-                download_txt(books_dir, book_id, parsed_book['title'])
-                download_image(images_dir, parsed_book['image'])
+                book_path = create_book_path(books_dir, parsed_book['title'])
+                image_url = get_image_url(book_soup)
+                image_path = create_image_path(images_dir, image_url)
+
+                download_txt(book_path, book_id)
+                download_image(image_path, image_url)
+
+                parsed_book['book_path'] = str(book_path)
+                parsed_book['image_path'] = str(image_path)
+                parsed_books.append(parsed_book)
                 break
             except requests.exceptions.ConnectionError as connect_err:
-                print(f'Connection failure: {connect_err}; book ID: {book_id}')
+                print(f'Connection failure: {connect_err};')
+                print(f'book ID: {book_id}')
                 if first_reconnection:
                     print('Retry in 5 seconds')
                     sleep(5)
@@ -117,6 +133,7 @@ def download_books_and_images(start: int, end: int):
             except requests.HTTPError as err:
                 print(err)
                 break
+    return parsed_books
 
 
 def main():
@@ -124,7 +141,8 @@ def main():
     parser.add_argument('-s', '--start_id', type=int, default=1)
     parser.add_argument('-e', '--end_id', type=int, default=10)
     args = parser.parse_args()
-    download_books_and_images(args.start_id, args.end_id)
+    book_ids = range(args.start_id, args.end_id + 1)
+    download_books_and_images(book_ids)
 
 
 if __name__ == '__main__':
--- parse_tululu_category.py
+++ parse_tululu_category.py
@@ -8,22 +8,31 @@ import requests
 from bs4 import BeautifulSoup
 from pathvalidate import sanitize_filename
 
-
-def save_pretty_json(info: list):
-    with open('books_info.json', 'w') as file:
-        file.write(json.dumps(info, sort_keys=False,
-                              indent=4, ensure_ascii=True))
+from parse_tululu import download_books_and_images
 
 
-def main():
-    base_url = 'https://tululu.org/l55/'
-    for page in range(1, 11):
-        page_url = f'https://tululu.org/l55/{page}/'
+def save_pretty_json(data):
+    with open('downloaded_books.json', 'w', encoding='utf8') as file:
+        file.write(json.dumps(data, indent=4, ensure_ascii=False))
+
+
+def get_book_ids(genre_url: str, start_page: int, end_page: int) -> list:
+    book_ids = []
+    for page in range(start_page, end_page + 1):
+        page_url = f'{genre_url}{page}/'
         response = requests.get(page_url)
         response.raise_for_status()
         soup = BeautifulSoup(response.text, 'lxml')
-        for link in soup.find('body').find_all('table', class_='d_book'):
-            print(urljoin(base_url, link.find('a')['href']))
+        for book in soup.find('body').find_all('table', class_='d_book'):
+            book_ids.append(book.find('a')['href'].strip('/b'))
+    return book_ids
+
+
+def main():
+    sci_fi_url = 'https://tululu.org/l55/'
+    book_ids = get_book_ids(sci_fi_url, 1, 4)
+    downloaded_books_catalog = download_books_and_images(book_ids)
+    save_pretty_json(downloaded_books_catalog)
 
 
 if __name__ == '__main__':
