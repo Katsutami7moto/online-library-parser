8107380b1b5ce4be992ace5ae9aeb1addbf881c7
Katsutami7moto <nclsf87@gmail.com>
2022-07-17 00:03:48 +0300

    Make pagination work

--- render_website.py
+++ render_website.py
@@ -23,14 +23,21 @@ def on_reload():
     pages_path = Path('pages')
     pages_path.mkdir(parents=True, exist_ok=True)
     template = env.get_template('template.html')
-    catalog = list(chunked(get_books_catalog('media'), 2))
-    paged_catalog = list(chunked(catalog, 6))
-    pages = list(range(1, len(paged_catalog) + 1))
+    paged_catalog = list(chunked(chunked(get_books_catalog('media'), 2), 6))
+    pages_links = {number: f'./index{number}.html'
+                   for number in range(1, len(paged_catalog) + 1)}
     for number, page in enumerate(paged_catalog, 1):
+        previous_page = f'./index{number - 1}.html' if number > 1 else None
+        next_page = (f'./index{number + 1}.html'
+                     if number < len(paged_catalog) else None)
+        page_title = f'Собрание НФ-худлита, страница {number}'
         rendered_page = template.render(
+            current_page_num=number,
             catalog=page,
-            page_title=f'Собрание НФ-худлита, страница {number}',
-            pages=pages
+            page_title=page_title,
+            pages_links=pages_links,
+            previous_page=previous_page,
+            next_page=next_page
         )
         file_path = pages_path.joinpath(f'index{number}.html')
         with open(file_path, 'w', encoding="utf8") as file:
